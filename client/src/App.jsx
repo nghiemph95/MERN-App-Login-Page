c2cb52e9a40a95e5e888ad38dba8eef87cfde6c0
Nghiem Pham <nghiemph95@gmail.com>
2023-05-07 15:06:23 +0700

    Updated: 07-05-2023 15:06:23

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
